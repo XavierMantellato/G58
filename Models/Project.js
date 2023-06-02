73a357e7e8d23f7c54cc457823bd427d455d8bb8
Xavier MANTELLATO <xavier.mantellato@arkance-systems.com>
2023-06-02 09:21:00 +0200

    test get psets/link

--- Models/Project.js
+++ Models/Project.js
@@ -1,15 +1,13 @@
 export class ProjectClass {
     // Constructors
 
-    constructor(id, name, rootId){
+    constructor(id, name){
         this.id = id;
         this.name = name;
-        this.rootFolder = rootId;
     };
 
     constructor(){
         this.id = "default";
         this.name = "default";
-        this.rootFolder = "default";
-        }
+        };
 }
