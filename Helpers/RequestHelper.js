913b2203579b366eabcb518d09360d209560671f
Xavier MANTELLATO <xavier.mantellato@arkance-systems.com>
2023-07-03 10:09:32 +0200

    test entity info

--- Helpers/RequestHelper.js
+++ Helpers/RequestHelper.js
@@ -1,6 +1,12 @@
-async function GetRequest(url, params) {
-
-    const response = await fetch(url + '?' + new URLSearchParams(params));
+async function GetRequest(url, headers, params) {
+    var request = url;
+    if (params != null){
+        request = request.concat('?' + URLSearchParams(params))
+    }
+    const response = await fetch(request, {
+        method: "GET",
+        headers : headers
+    })
     const data = await response.json();
 
     return data;
